8aac441f7154ecf75c92a863f90ef15a16c9ee98
developeruno <kumawatarjun1012@gmail.com>
2020-02-23 16:19:08 +0530

    web-server

---
+++ src/hack/fields/Person.java
@@ -0,0 +1,11 @@
+package hack.fields;
+
+public class Person {
+	public Long id;
+	public String name;
+	public String phone;
+	public String password;
+	public String state;
+	public String district;
+	public String role;
+}
